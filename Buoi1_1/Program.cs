c012cba25ffe79fe5372f0207e09133d788367cf
PhamTranAnh <Anhphamtran2002@gmail.com>
2023-09-23 20:13:23 +0700

    phamtrananh

---
+++ Buoi1_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, tong, hieu, tich;
+            double thuong = 0;
+            Console.Write("Nhap so a:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap so b:");
+            b = Convert.ToInt32(Console.ReadLine());
+            tong = a + b;
+            hieu = a - b;
+            tich = a * b;
+            if (b != 0)
+            {
+                thuong = (double)a / b;
+            }
+            Console.WriteLine("Tong = " + tong);
+            Console.WriteLine("Hieu = {0} ", hieu);
+            Console.WriteLine("Tich = {0} ,Thuong= { 1}",tich,thuong);
+            Console.ReadKey();
+        }
+    }
+
+    }
+
---
+++ Buoi1_3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int answer = 0;
+            for (int i = 1; i <= 2*n-1; i+=2)
+            {
+                answer += i;
+            }
+            Console.WriteLine(answer);
+            Console.ReadKey();
+        }
+    }
+}
